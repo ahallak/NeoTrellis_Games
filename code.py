17d7d747e2efff3983f74f975ea1006741eddf37
ahallak <a.hallak137@gmail.com>
2024-02-01 15:14:07 -0500

    Allow both games without editing code

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import board
 from adafruit_neotrellis.neotrellis import NeoTrellis
-from memory_game import Memory_Game
+from memory_match import Memory_Match
 from trellis_says import Trellis_Says
 
 BRIGHTNESS = 0.5
@@ -10,5 +10,6 @@ i2c_bus = board.I2C()
 trellis = NeoTrellis(i2c_bus)
 trellis.brightness = BRIGHTNESS
 
-#game = Memory_Game(trellis, NUM_TRELLIS)
-game = Trellis_Says(trellis, NUM_TRELLIS)
+while(True):
+    game = Memory_Match(trellis, NUM_TRELLIS)
+    game = Trellis_Says(trellis, NUM_TRELLIS)
